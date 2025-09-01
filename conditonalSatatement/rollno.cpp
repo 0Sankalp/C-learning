8f36d174bb2823b0113483bd17d813aba735058f
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-09-01 23:04:12 +0530

    basic program for understaing condiotnal statemet by using roll no

---
+++ conditonalSatatement/rollno.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int rollno;
+    cout<<"Enter your roll no";
+    cin>>rollno;
+    if (rollno<1)
+    {
+
+        cout<<"Inavalid user";
+
+    }
+    else
+    {
+
+        cout<<"Valid user";
+        }
+        return 0;
+}
