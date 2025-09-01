2c038b414b5f49062a138b6f4dd16122e33fb54d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2025-09-01 23:01:26 +0530

    Finding max of two number

---
+++ conditonalSatatement/main.cpp
@@ -0,0 +1,24 @@
+// Finding max of two number
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"enter 2 no";
+    cin>>x>>y;
+
+    if(x>y)
+    {
+        cout<<"maxis x";
+    }
+    else{
+        cout<<"max is y";
+    }
+    return 0;
+}
+
+//nter 2 no2 4
+//max is y
